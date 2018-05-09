270d3caa7bc0b42c7ef5a3ccc6e837c7fbd81bd3
gabi200 <29951139+gabi200@users.noreply.github.com>
2018-05-09 19:00:33 +0300

    v1.0.3 : bug fixes and better support across OS-es

--- passgen.py
+++ passgen.py
@@ -1,34 +1,31 @@
-import os,random
+import os
+import random
 
 words = ["cat","me","game","dog","Lol","haha","xD","x","super","royale","python","boom"]
 number = random.randint(36,861)
 number1 = random.randint(56,483)
-symbols = ["!","?","@","#","&","€","_","*","^"]
+symbols = ["!","?","@","#","&","€","_","*","^"] 
 bar = "-" * 32
 
 r = random.randint(0,11)
 r1 = random.randint(0,8)
 r2 = random.randint(1,3)
 
-if r in [0,1,2,3,4,5,6,7,8,9,10,11]:
-	rw = words[r]
-if r1 in [0,1,2,3,4,5,6,7,8]:
-	rs = symbols[r1]
-
-if r2 == 1:
-	password = rw + rs + str(number)
-if r2 == 2:
-	password = str(number) + rs + rw + rs
-if r2 == 3:
-	password = rs + str(number) + rw + str(number1)
-if r2 == 4:
-	password = rw + str(number1) + rs + str(number)
-
-print("Random Password Generator v1.0.2")
+rw = words[r]
+rs = symbols[r1] 
+if r2 == 1: 	
+    password = rw + rs + str(number)
+if r2 == 2: 
+    	password = str(number) + rs + rw + rs
+if r2 == 3: 
+    	password = rs + str(number) + rw + str(number1)
+if r2 == 4: 
+    	password = rw + str(number1) + rs + str(number)
+print("Random Password Generator v1.0.3")
 print("by gabi200")
-print(bar)
+print(bar) 
 input("Press ENTER to generate a password...")
-os.system("clear")
+os.system('cls' if os.name == 'nt' else 'clear')
 print("Here's your password:")
 print(bar)
 print(password)
